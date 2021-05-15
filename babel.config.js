c5d35d36c40f238f00a0031df918b01b95aa09b8
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 17:08:11 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = api => {
       "@babel/plugin-proposal-nullish-coalescing-operator",
       "@babel/plugin-proposal-optional-chaining"
     ],
-    presets [
       [
         "@babel/preset-env",
         {
