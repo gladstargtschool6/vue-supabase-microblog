80eeed9a442da601266f7e54fb8aa410efa1c0fe
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 17:10:30 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,6 @@ module.exports = api => {
     ],
       [
         "@babel/preset-env"
-        {
           useBuiltIns: "entry",
           // caller.target will be the same as the target option from webpack
           targets: api.caller(caller => caller && caller.target === "node")
