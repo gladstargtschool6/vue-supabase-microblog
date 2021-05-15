9038a981b572991886ae8584c7c92e58ffa2b806
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 16:45:36 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
-import express from "express";
-const app = express();
-const port = 8080;
 
-app.get('/',(req,res)=> {
-  res.send('Express Home page')
-});
-
-app.listen(port,() => console.log(`Server started running on ${port}`));
 {
     "presets": [
         "@babel/preset-env"
