319e84ceb3e109f7a03444e4f965d9d82262ad9f
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 17:04:28 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,24 @@
-{
-  "presets" [
-    [
-      "next/babel",
-      {
-        "preset-env": {},
-        "transform-runtime": {},
-        "styled-jsx": {},
-        "class-properties": {}
-      }
+// babel.config.js
+
+module.exports = api => {
+  return {
+    plugins: [
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-optional-chaining"
+    ],
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "entry",
+          // caller.target will be the same as the target option from webpack
+          targets: api.caller(caller => caller && caller.target === "node")
+            ? { node: "current" }
+            : { chrome: "58", ie: "11" }
+        }
+      ]
     ]
-  ],
-  "plugins" [
+  }
+}
+
 
