00d2022c34002d8e8f3664890eeb8a4072f48845
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 18:01:20 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
-    '@vue/cli-plugin-babel/preset'
+  pages: {
+    'index': {
+      entry: './src/main.js',
+      template: 'public/index.html',
+      title: 'Welcome to my vue generator project',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    /* Disabled - Only one time
+    'googleVerify': {
+      entry: './src/error-instance.ts',
+      template: 'public/somelink.html',
+      title: 'Error page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    */
+
+  },
+  devServer: {
+    'port': 3000
+  },
+  css: {
+    sourceMap: false
+  },
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
 }
